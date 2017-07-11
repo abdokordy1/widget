b7de8b58dd8b316fd829f6a3ddb92e11aab6e12d
Abdallah <abdullah.kordy@gmail.com>
2017-07-11 14:38:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/appwidget/ExampleAppWidgetConfigure.java
@@ -0,0 +1,16 @@
+package com.example.android.appwidget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class ExampleAppWidgetConfigure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button zsd =(Button) findViewById(R.id.button);
+
+    }
+}
